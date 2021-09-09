75045e01fbd4514fad0e341bc18123e10ef6888d
earthlymeg <meganshulmister@gmail.com>
2021-09-09 10:59:33 -0600

    retrieving last 10 blocks

--- src/components/RecentBlocks.js
+++ src/components/RecentBlocks.js
@@ -4,21 +4,48 @@ export default function RecentBlocks() {
     var web3 = new Web3("https://mainnet.infura.io/v3/8037227268fb45909c7a6ca8f7778194");
 
 
-    // const getRecentBlocks = async function () {
-    //     // const latest = await web3.eth.getBlockNumber();
-    //     for (var i=0; i < 10; i++) {
-    //         console.log(web3.eth.getBlock(web3.eth.blockNumber - i));
-    //       }
+    const getRecentBlocks = async function () {
+        let latest = await web3.eth.getBlockNumber();
+        let range = [];
+        const batch = new web3.eth.BatchRequest()
 
+        for (let i = 10; i > 0; i--) {
+            range.push(latest);
+        
+            latest--;
+        }
 
-    // }
+      
+        const total = range.length;
+        let counter = 0;
+        let blocks = [];
+      
+        await new Promise(function(resolve, reject){
+          range.forEach(blockNumber => {
+            batch.add(
+              web3.eth.getBlock.request(blockNumber, (error, data) => {
+                if (error) return reject(error);
+      
+                counter++;
+                blocks.push(data);
+      
+                if (counter === total) resolve();
+              })
+            )
+          });
+      
+          batch.execute()
+        });
+      
+        return blocks
 
-    for (var i=0; i < 10; i++) {
-        console.log(web3.eth.getBlock(web3.eth.blockNumber - i));
-      }
-
+    }
+    
+    getRecentBlocks().then(console.log)
+    
     return (
         <div>
+            
             <h1>Recent Blocks</h1>
         </div>
     )
